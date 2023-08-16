673af9da3a45ece731f1e86bbf1cca8073dd796e
annac@NITRO_ACR <annaclararenno@gmail.com>
2023-08-16 16:24:26 -0300

    Codigo desenvolvido durante a aula 2

---
+++ Aula2_IntroducaoJava/src/main/java/org/example/Main.java
@@ -0,0 +1,79 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+      //INTRODUCAO JAVA
+        /*
+
+
+         */
+
+        //variaveis
+        int valor;
+        //double saldo;
+
+        //constante
+        //int idade =20;
+
+        /*
+        string é uma classe!!!!!
+        String nome = "Silas";
+        System.out.println(nome);
+
+
+        double altura = 1.70;
+        float altura2;
+
+        altura2 = (float) altura;
+
+        System.out.println(altura2);
+
+
+
+        Scanner cin = new Scanner(System.in);
+
+        System.out.println("Entre com sua idade");
+        int idade = cin.nextInt();
+
+        cin.nextLine();
+
+        System.out.println("Entre com seu nome");
+        String nomealuno = cin.nextLine();
+
+        System.out.println("Entre com seu peso");
+        double peso = cin.nextDouble();
+
+        //cin.nextLine();
+
+
+
+        //variavel para if else
+        int deposito =100;
+        int transferencia = 100;
+        int saldo = deposito+transferencia;
+
+        if(saldo >= 200)
+            System.out.println("Voce consegue comprar!");
+        else
+            System.out.println("Falta dinheiro!!");
+
+
+
+        //for basico para contagem de 1 a 10
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i+1);
+
+        }
+
+         */
+
+
+
+
+
+
+    }
+}
